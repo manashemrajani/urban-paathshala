6ef213d31d470a1bb26cd568d26596e1174b2d02
Manas Hemrajani <manashemrajani@UCLP1279.local>
2020-04-04 12:32:52 +0530

    Updates

---
+++ precache-manifest.41acbedbf80685ac030d52cebb658c8d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0366bab0b102242e5f9de0133b6719fa",
+    "url": "/urban-paathshala/index.html"
+  },
+  {
+    "revision": "781c4d2661cd3632110f",
+    "url": "/urban-paathshala/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "fbd58423063fc6b1b68c",
+    "url": "/urban-paathshala/static/js/2.6335b169.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/urban-paathshala/static/js/2.6335b169.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "781c4d2661cd3632110f",
+    "url": "/urban-paathshala/static/js/main.bd4c7372.chunk.js"
+  },
+  {
+    "revision": "74d9203bd6f132c5e797",
+    "url": "/urban-paathshala/static/js/runtime-main.7866f015.js"
+  }
+]);
